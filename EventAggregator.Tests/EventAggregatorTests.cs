7acf4efeb94db8b845e28027e8fe284c68db3df9
Paul Heasley <paul.heasley@gmail.com>
2015-07-14 22:42:03 +1000

    Passing normal execution test

--- EventAggregator.Tests/EventAggregatorTests.cs
+++ EventAggregator.Tests/EventAggregatorTests.cs
@@ -13,7 +13,7 @@ namespace EventAggregator.Tests
             // Arrange
 
             var eventAggregator = new EventAggregator();
-            var observer1 = new Mock<IObserver<TestEvent>>();
+            var observer1 = new Mock<IObserve<TestEvent>>();
             var observer2 = new Mock<IObserve<OtherEvent>>();
 
             eventAggregator.Subscribe(observer1.Object);
@@ -29,12 +29,17 @@ namespace EventAggregator.Tests
 
             // Assert
 
-            observer1.Verify(x => x.OnNext(It.IsAny<IEvent>()), Times.Exactly(2));
+            observer1.Verify(x => x.OnNext(It.IsAny<IEvent>()), Times.Once);
             observer1.Verify(x => x.OnNext(testEvent), Times.Once);
             observer2.Verify(x => x.OnNext(otherEvent), Times.Once);
         }
 
         public void ObserverStopsReceivingEventsWhenUnsubscribed()
+        {
+            
+        }
+
+        public void BaseObserverReceivesAllEvents()
         {
             
         }
--- EventAggregator/EventAggregator.cs
+++ EventAggregator/EventAggregator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EventAggregator
 {
@@ -7,14 +8,79 @@ namespace EventAggregator
     {
         private readonly List<IEvent> _events;
         private readonly List<IObserver<IEvent>> _observers;
+        private readonly Dictionary<Type, List<IObserver<IEvent>>> _typedObservers;
 
         public EventAggregator()
         {
             _events = new List<IEvent>();
             _observers = new List<IObserver<IEvent>>();
+            _typedObservers = new Dictionary<Type, List<IObserver<IEvent>>>();
         }
 
+        /// <summary>
+        /// Subscribes to all events (in the case of IObserver&lt;IEvent&gt;)
+        /// </summary>
+        /// <param name="observer">The subscriber.</param>
+        /// <returns>An Unsubscriber to allow unsubscribing from events</returns>
         public IDisposable Subscribe(IObserver<IEvent> observer)
+        {
+            return SubscribeAll(observer);
+        }
+
+        /// <summary>
+        /// Subscribes to all events (in the case of IObserve&lt;IEvent&gt;)
+        /// </summary>
+        /// <param name="observer">The subscriber.</param>
+        /// <returns>An Unsubscriber to allow unsubscribing from events</returns>
+        public IDisposable Subscribe(IObserve<IEvent> observer)
+        {
+            return SubscribeAll(observer);
+        }
+        
+        /// <summary>
+        /// Subscribes to a specific event type.
+        /// </summary>
+        /// <typeparam name="T">The event type to subscribe to.</typeparam>
+        /// <param name="observer">The subscriber.</param>
+        /// <returns>An Unsubscriber to allow unsubscribing from events</returns>
+        public IDisposable Subscribe<T>(IObserve<T> observer) where T : IEvent
+        {
+            // Check whether observer is already registered. If not, add it 
+            List<IObserver<IEvent>> observers;
+            if (!_typedObservers.TryGetValue(typeof(T), out observers))
+                observers = _typedObservers[typeof(T)] = new List<IObserver<IEvent>>();
+            if (!observers.Contains(observer))
+            {
+                observers.Add(observer);
+                // Provide observer with existing data. 
+                foreach (var item in _events.Where(x => x is T))
+                    observer.OnNext(item);
+            }
+            return new Unsubscriber<IEvent>(_typedObservers[typeof(T)], observer);
+        }
+
+        /// <summary>
+        /// Publishes an event to subscribers
+        /// </summary>
+        /// <param name="message">The event to publish, can be any implementation of IEvent.</param>
+        public void Publish(IEvent message)
+        {
+            _events.Add(message);
+            foreach (var observer in _observers)
+                observer.OnNext(message);
+
+            // Publish event to typed observers.
+            if (!_typedObservers.ContainsKey(message.GetType())) return;
+            foreach (var observer in _typedObservers[message.GetType()])
+                observer.OnNext(message);
+        }
+
+        /// <summary>
+        /// Subscribes to all events
+        /// </summary>
+        /// <param name="observer">The subscriber.</param>
+        /// <returns>An Unsubscriber to allow unsubscribing from events</returns>
+        private IDisposable SubscribeAll(IObserver<IEvent> observer)
         {
             // Check whether observer is already registered. If not, add it 
             if (!_observers.Contains(observer))
@@ -26,15 +92,11 @@ namespace EventAggregator
             }
             return new Unsubscriber<IEvent>(_observers, observer);
         }
-
-        public void Publish(IEvent message)
-        {
-            _events.Add(message);
-            foreach (var observer in _observers)
-                observer.OnNext(message);
-        }
     }
 
+    /// <summary>
+    /// Enables a subscriber to unsubscribe to events by calling Dispose().
+    /// </summary>
     internal class Unsubscriber<T> : IDisposable
     {
         private readonly List<IObserver<T>> _observers;
